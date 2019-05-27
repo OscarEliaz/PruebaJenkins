0cbb5074eaa48093b6370e37778d5db6fcdfee44
WTO Eliaz <oscar__x@hotmail.com>
2019-05-27 15:54:14 -0400

    gjhkkh

--- IxCacaoApp/IxCacaoApp/IxCacaoApp/Controlador/ControladorLogin.cs
+++ IxCacaoApp/IxCacaoApp/IxCacaoApp/Controlador/ControladorLogin.cs
@@ -14,7 +14,7 @@ namespace IxCacaoApp.Controlador
         SqlConnection conexion;
         SqlCommand comando;
         SqlDataReader lector;
-        static string nombreServidor = "DESKTOP-5UDNSII\\MSSQLSERVER02";
+        static string nombreServidor = "DESKTOP-5UDNSII\\MSSQLSERVER02/hgkkhk"jkl;
         static string nombreBD = "IXcacao";
         static string Username = "AdminCacaoApp";
         static string contrasena = "Cacao2018app";
