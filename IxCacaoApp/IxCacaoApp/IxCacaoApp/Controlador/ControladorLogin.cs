a3c55f3b8916c5f8a3f36cb120f3be07243a44b0
WTO Eliaz <oscar__x@hotmail.com>
2019-05-30 14:18:54 -0400

    cjkljklc

--- IxCacaoApp/IxCacaoApp/IxCacaoApp/Controlador/ControladorLogin.cs
+++ IxCacaoApp/IxCacaoApp/IxCacaoApp/Controlador/ControladorLogin.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace IxCacaoApp.Controlador/sdfzfdfzxzdfzszx;
+namespace IxCacaoApp.Controlador
 {
     class ControladorLogin
     {
